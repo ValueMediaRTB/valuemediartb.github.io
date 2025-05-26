7534b082debff3f310b7416c1061519d9178d133
ValueMediaRTB <rtbmanagment@valuemedia-ltd.com>
2025-05-26 11:42:31 +0300

    GetCampaigns

--- mainscripts.js
+++ mainscripts.js
@@ -25,7 +25,11 @@ let codeVerifier;
 let codeChallenge;
 let clientID;
 let token;
+let access_token;
+let refresh_token;
+let serverURL;
 const publisherID = 470796;
+const redirectURI = 'https://valuemediartb.github.io/auth.html'
 
 async function initializeCodes() {
       codeVerifier = generateRandomString(getRandomInt(43,128));
@@ -37,8 +41,12 @@ async function initializeCodes() {
 
 async function indexLoaded() {
     await initializeCodes();
-    document.getElementById('authorizeDaisyconBtn').disabled = false
+    document.getElementById('authorizeDaisyconBtn').disabled = false;
     sessionStorage.setItem('clientID', 0);
+    serverURL = sessionStorage.getItem('serverURL')
+    if(serverURL){
+        document.getElementById('serverURLInput').value = serverURL;
+    }
     document.getElementById('codeVerifierContainer').innerHTML = "Code verifier: "+codeVerifier;
     console.log("indexLoaded() called")
 }
@@ -60,18 +68,24 @@ function authorizeDaisycon(){
     // Get form values
     document.getElementById('authorizeDaisyconBtn').disabled = true
     clientID = document.getElementById('clientID').value;
-    sessionStorage.setItem('clientID', clientID);
-    const redirectURI = 'https://valuemediartb.github.io/auth.html'
+    serverURL = document.getElementById('serverURLInput').value;
     
     // Validate inputs
     if (!clientID) {
         alert('Client ID is missing!');
         return;
     }
+    if (!serverURL) {
+        alert('Server URL is missing!');
+        return;
+    }
     if(!redirectURI || !codeChallenge){
         alert('CodeVerifier or redirectURI are null!');
         return;
     }
+
+    sessionStorage.setItem('clientID', clientID);
+    sessionStorage.setItem('serverURL',serverURL);
 
     authorizeUrl = new URL('https://login.daisycon.com/oauth/authorize');
     authorizeUrl.searchParams.append('response_type','code');
@@ -83,9 +97,6 @@ function authorizeDaisycon(){
 
 }; 
 async function accessDaisycon(){
-    // Get form values
-    const token = document.getElementById('tokenProcessed').value;
-    const redirectURI = 'https://valuemediartb.github.io/auth.html'
 
     // Validate inputs
     if (!token) {
@@ -99,6 +110,51 @@ async function accessDaisycon(){
 
     accessUrl = 'https://login.daisycon.com/oauth/access-token';
     const formData = {'grant_type':'authorization_code',
+        'code':token,
+        'client_id':clientID,
+        'client_secret':'',
+        'redirect_uri':redirectURI,
+        'code_verifier':codeVerifier
+    }
+    try {
+        const response = await fetch(`${serverURL}/proxy` , {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            targetUrl: 'https://login.daisycon.com/oauth/access-token',
+            body: formData,
+            headers: { 'Content-Type': 'application/json' },
+            method:"POST"
+          })
+        });
+    
+        const data = await response.json();
+        // Handle the response (e.g., save access token)
+        document.getElementById('accessResult').innerHTML = "Authentication successful!"
+        document.getElementById('accessToken').innerHTML = "Access token: "+data.access_token
+        document.getElementById('refreshToken').innerHTML = "Refresh token: "+data.refresh_token
+        document.getElementById('accessDaisyconBtn').disabled = true
+        document.getElementById('getCampaignMaterialBtn').disabled = false
+
+        console.log('Success:', data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+}
+async function refreshAccessDaisycon(){
+
+    // Validate inputs
+    if (!token) {
+        alert('Token is missing!');
+        return;
+    }
+    if(!redirectURI || !codeVerifier){
+        alert('CodeVerifier or redirectURI are null!');
+        return;
+    }
+
+    accessUrl = 'https://login.daisycon.com/oauth/access-token';
+    const formData = {'grant_type':'refresh_token',
         'code':token,
         'client_id':clientID,
         'client_secret':'',
@@ -134,10 +190,10 @@ async function accessDaisycon(){
 async function getCampaignMaterial(){
     if(token){
         try {
-            const response = await fetch('https://e9ff-91-132-4-72.ngrok-free.app/proxy' , {
+            const response = await fetch(`${serverURL}/proxy` , {
             method: 'POST',
             body: JSON.stringify({
-                targetUrl:'https://services.daisycon.com/publishers/470796/material/programs?page=1&per_page=5',
+                targetUrl:`https://services.daisycon.com/publishers/${publisherID}/material/programs?page=1&per_page=5`,
                 headers: { 'accept': 'application/json',
                 'Authorization':'Bearer '+token },
                 method:"GET"
